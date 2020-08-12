9af3e504888b516984ee06bc5a6ea238211e6405
divyanshu <divyanshusonwani@hotmail.com>
2020-08-12 21:21:37 +0530

    unit testing

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from app.calc import add, subtract
+
+
+class CalcTests(TestCase):
+
+    def test_add_numbers(self):
+        """Test that two numbers are added"""
+        self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """Test that subtract numbers are added"""
+        self.assertEqual(subtract(3, 2), 1)
